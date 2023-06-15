7b8aa711dd84b31d7b47a40f148e5975e5202faf
Mansur Zamaletdinov <zamaletdinov98@list.ru>
2023-06-15 17:27:59 +0300

    HomeworkComit

--- Lambda function/InnovationsInC++/InnovationsInC++.cpp
+++ Lambda function/InnovationsInC++/InnovationsInC++.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Ru");
 
     std::vector<int> Vector = { 4, 7, 9, 14, 12 };
@@ -14,19 +13,18 @@ int main()
         std::cout << a << " ";
     };
 
-    for (const int & value : Vector) {
-        func(value);
-    }
+    std::for_each(Vector.begin(), Vector.end(), func);
 
+    
     std::cout << std::endl;
     std::cout << "Выходные данные: ";
 
-    for (int value : Vector) {
-        if (value % 2 != 0) {
-            (value) *= 3;
+    std::for_each(Vector.begin(), Vector.end(), [&Vector](int& i) {
+        if (i % 2 != 0) {
+            (i) *= 3;
         }
-        func(value);
-    }
+    });
 
+    std::for_each(Vector.begin(), Vector.end(), func);
     std::cout << std::endl;
 }
--- OperationsOnCorners/OperationsOnCorners/OperationsOnCorners.cpp
+++ OperationsOnCorners/OperationsOnCorners/OperationsOnCorners.cpp
@@ -1,23 +1,44 @@
-﻿#include <iostream>
-#include <math.h>
+﻿#include <math.h>
 #include <functional>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
+const double PI = 3.1415926;
+
+void FooSin(int angle)
+{
+	std::cout << " sin: " << sin((angle * PI) / 180.0) << '\t';
+};
+
+void FooCos(int angle)
+{
+	std::cout << " cos: " << cos((angle * PI) / 180.0) << '\t';
+};
+
+void FooTan(int angle)
+{
+	std::cout << "tan: " << tan((angle * PI) / 180.0) << '\t';
+};
+
+
 
 int main() {
 	setlocale(LC_ALL, "Ru");
 
-	int pi = 3.1415926;
+	double pi = 3.1415926;
 
-	std::vector<double> angles{30, 60, 90};
+	std::vector<double> angles{ 30, 60, 90 };
 
 	std::cout << "[Входные данные]: ";
 	std::for_each(angles.begin(), angles.end(), [&angles](double& n) {
 		std::cout << n << " * 3.1415926 / 180";
 		(n *= 3.1415926 / 180);
-	});
+		});
 	std::cout << std::endl;
 	std::cout << "[Выходные данные]: " << std::endl;
+
+
 
 	int type = 0;
 	std::function<void(double)> funcs = [&type](const double& angle) {
